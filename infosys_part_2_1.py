818e921fe31075f01f6d7af103b2ba5a20d9a658
subham rai <34444611+subhamkrai@users.noreply.github.com>
2019-01-08 13:22:39 +0530

    Update infosys_part_2_1.py

--- infosys_part_2_1.py
+++ infosys_part_2_1.py
@@ -1,5 +1,5 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np 
+import pandas as pd 
 import warnings
 warnings.filterwarnings('ignore')
 from bokeh.plotting import figure, output_file, show, output_notebook
